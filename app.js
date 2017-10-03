98ecbf599443205aa4c34bdf32733ba28c7bc83c
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-10-03 14:36:12 -0700

    chore: Initial refactor

--- app.js
+++ app.js
@@ -11,63 +11,63 @@ var readers = require('./lib/readers');
 var writer = require('./lib/writer')(config);
 
 function fetchDataForServer(requestion, server, lastMetrics) {
-	RQ.sequence([
-		readers.connectToMongoDb,
-		readers.fetchServerStatus,
-		writer.toGraphiteMetricsArray(server, config.metrics.serverStatus, lastMetrics),
-		writer.sendToGraphite
-	])(requestion, server);
+  RQ.sequence([
+    readers.connectToMongoDb,
+    readers.fetchServerStatus,
+    writer.toGraphiteMetricsArray(server, config.metrics.serverStatus, lastMetrics),
+    writer.sendToGraphite
+  ])(requestion, server);
 }
 
 function fetchReplicaStatus(requestion, server) {
-	RQ.sequence([
-		readers.connectToMongoDb,
-		readers.replSetGetStatus,
-		writer.replicaSetToGraphiteMetrics,
-		writer.sendToGraphite
-	])(requestion, server);
+  RQ.sequence([
+    readers.connectToMongoDb,
+    readers.replSetGetStatus,
+    writer.replicaSetToGraphiteMetrics,
+    writer.sendToGraphite
+  ])(requestion, server);
 }
 
 var lastResultArray = [];
 
 function intervalLoop() {
 
-	var	workArray = config.servers.map(function(server, index) {
-		return function(requestion) {
-			fetchDataForServer(requestion, server, lastResultArray[index]);
-		};
-	});
+  var workArray = config.servers.map(function(server, index) {
+    return function(requestion) {
+      fetchDataForServer(requestion, server, lastResultArray[index]);
+    };
+  });
 
-	RQ.parallel(workArray)(function(success, failure) {
-		if (failure) {
-			console.log('Error:', failure);
-		}
-		else {
-			lastResultArray = success;
-			console.log('ServerStatus fetched and set to graphite');
-		}
-	});
+  RQ.parallel(workArray)(function(success, failure) {
+    if (failure) {
+      console.log('Error:', failure);
+    }
+    else {
+      lastResultArray = success;
+      console.log('ServerStatus fetched and set to graphite');
+    }
+  });
 
-	var clusters = _.values(_.groupBy(config.servers, 'cluster'));
-	var work = clusters.map(function(servers) {
+  var clusters = _.values(_.groupBy(config.servers, 'cluster'));
+  var work = clusters.map(function(servers) {
 
-		var	replicaWork = servers.map(function(server, index) {
-			return function(requestion) {
-				fetchReplicaStatus(requestion, server);
-			};
-		});
+    var replicaWork = servers.map(function(server, index) {
+      return function(requestion) {
+        fetchReplicaStatus(requestion, server);
+      };
+    });
 
-		return RQ.fallback(replicaWork);
-	});
+    return RQ.fallback(replicaWork);
+  });
 
-	RQ.parallel(work)(function(success, failure) {
-		if (failure) {
-			console.log('Error:', failure);
-		}
-		else {
-			console.log('ReplicaSetStatus fetched and set to graphite');
-		}
-	});
+  RQ.parallel(work)(function(success, failure) {
+    if (failure) {
+      console.log('Error:', failure);
+    }
+    else {
+      console.log('ReplicaSetStatus fetched and set to graphite');
+    }
+  });
 }
 
 setInterval(intervalLoop, config.intervalSeconds * 1000);
--- tests/writer-specs.js
+++ tests/writer-specs.js
@@ -1,81 +1,81 @@
 var should = require('should');
 
 describe('when transforming mongo db metrics', function() {
-	var result, secondResult;
+  var result, secondResult;
 
-	var serverStatus = require('./serverStatus.json');
-	var metricMap = {
-		connections: { current: "gauge", available: "gauge" },
-		backgroundFlushing: { last_ms: "gauge" },
-		asserts: { user: "counter" }
-	};
+  var serverStatus = require('./serverStatus.json');
+  var metricMap = {
+    connections: { current: "gauge", available: "gauge" },
+    backgroundFlushing: { last_ms: "gauge" },
+    asserts: { user: "counter" }
+  };
 
-	var server = {
-		cluster: 'muppet',
-		host: 'muppet.prod.app.com'
-	};
+  var server = {
+    cluster: 'muppet',
+    host: 'muppet.prod.app.com'
+  };
 
-	var config = {
-		graphiteKeyTemplateGauges: 'test.databases.<%= cluster %>.<%= host %>.gauges.<%= metric %>',
-		graphiteKeyTemplateCounters: 'test.databases.<%= cluster %>.<%= host %>.counters.<%= metric %>.count'
-	};
+  var config = {
+    graphiteKeyTemplateGauges: 'test.databases.<%= cluster %>.<%= host %>.gauges.<%= metric %>',
+    graphiteKeyTemplateCounters: 'test.databases.<%= cluster %>.<%= host %>.counters.<%= metric %>.count'
+  };
 
-	var writer = require('../lib/writer')(config);
-	var RQ = require('../rq');
+  var writer = require('../lib/writer')(config);
+  var RQ = require('../rq');
 
-	before(function(done) {
-		done();
-	});
+  before(function(done) {
+    done();
+  });
 
-	describe('without short hostname default', function() {
+  describe('without short hostname default', function() {
 
-		before(function(done) {
-			writer.toGraphiteMetricsArray(server, metricMap)
-				(function(success, failure) {
-					result = success;
+    before(function(done) {
+      writer.toGraphiteMetricsArray(server, metricMap)
+        (function(success, failure) {
+          result = success;
 
-					serverStatus.asserts.user = 1053 + 10;
-					writer.toGraphiteMetricsArray(server, metricMap, result)
-						(function (success2, failure2) {
-							secondResult = success2;
-							done();
-						}, serverStatus);
-				}, serverStatus);
-		});
+          serverStatus.asserts.user = 1053 + 10;
+          writer.toGraphiteMetricsArray(server, metricMap, result)
+            (function (success2, failure2) {
+              secondResult = success2;
+              done();
+            }, serverStatus);
+        }, serverStatus);
+    });
 
-		it('should transform to array with flat key value', function(done) {
-			result.keyValues['test.databases.muppet.muppet_prod_app_com.gauges.connections_current'].should.equal(145);
-			result.keyValues['test.databases.muppet.muppet_prod_app_com.gauges.backgroundFlushing_last_ms'].should.equal(25);
-			done();
-		});
+    it('should transform to array with flat key value', function(done) {
+      result.keyValues['test.databases.muppet.muppet_prod_app_com.gauges.connections_current'].should.equal(145);
+      result.keyValues['test.databases.muppet.muppet_prod_app_com.gauges.backgroundFlushing_last_ms'].should.equal(25);
+      done();
+    });
 
-		it('should only send counter deltas', function(done) {
-			result.keyValues['test.databases.muppet.muppet_prod_app_com.counters.asserts_user.count'].should.equal(0);
-			secondResult.keyValues['test.databases.muppet.muppet_prod_app_com.counters.asserts_user.count'].should.equal(10);
-			done();
-		});
-	});
-
-
-	describe('with short hostname default', function() {
-
-		before(function(done) {
-			server.shortName = "shorty";
-			var func = writer.toGraphiteMetricsArray(server, metricMap);
-
-			func(function(success, failure) {
-				result = success;
-				done();
-			}, serverStatus);
-		});
+    it('should only send counter deltas', function(done) {
+      result.keyValues['test.databases.muppet.muppet_prod_app_com.counters.asserts_user.count'].should.equal(0);
+      secondResult.keyValues['test.databases.muppet.muppet_prod_app_com.counters.asserts_user.count'].should.equal(10);
+      done();
+    });
+  });
 
 
-		it('should use shortname instead of host', function(done) {
-			result.keyValues['test.databases.muppet.shorty.gauges.connections_current'].should.equal(145);
-			result.keyValues['test.databases.muppet.shorty.gauges.backgroundFlushing_last_ms'].should.equal(25);
-			done();
-		});
+  describe('with short hostname default', function() {
 
-	});
+    before(function(done) {
+      server.shortName = "shorty";
+      var func = writer.toGraphiteMetricsArray(server, metricMap);
+
+      func(function(success, failure) {
+        result = success;
+        done();
+      }, serverStatus);
+    });
+
+
+    it('should use shortname instead of host', function(done) {
+      result.keyValues['test.databases.muppet.shorty.gauges.connections_current'].should.equal(145);
+      result.keyValues['test.databases.muppet.shorty.gauges.backgroundFlushing_last_ms'].should.equal(25);
+      done();
+    });
+
+  });
 
 });
